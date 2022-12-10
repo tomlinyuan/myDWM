d5755789588dca4edcd1ca3d7560642d50f76e49
tomlinyuan <1902734565@qq.com>
2022-12-10 14:25:58 +0800

    fix font and add hotkeys

--- config.h
+++ config.h
@@ -5,7 +5,10 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10",
+					"WenQuanYi Micro Hei:size=10:type=Regular:antialias=true:autohint=true",
+					"JoyPixels:pixelsize=14:type=Regular:antialias=true:autohint=true",
+                                        "Symbols Nerd Font:pixelsize:12:type=2048-em:antialias=true:autohint=ture"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -19,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -62,6 +65,9 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_F1,     spawn,          SHCMD("amixer sset Master toggle") },
+	{ MODKEY,                       XK_F2,     spawn,          SHCMD("amixer sset Master 5%- unmute") },
+	{ MODKEY,                       XK_F3,     spawn,          SHCMD("amixer sset Master 5%+ unmute") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
